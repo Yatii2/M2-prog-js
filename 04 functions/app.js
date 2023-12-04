3481f5977a5094608a3545af42d01acd7c656c19
Yati <37732@ma-web.nl>
2023-12-04 14:40:52 +0100

    04 4

--- 04 functions/app.js
+++ 04 functions/app.js
@@ -48,7 +48,20 @@ class App
     {
         console.log("hello world!");
     }
+    newClassFunction()
+    {
+        console.log("hello world in de nieuwClassFunction");
+    }
+    anotherFunctionWithArguments(aArgument)
+    {
+        console.log("kom maar met je argument");
+        console.log("hier:");
+        console.log(aArgument);
+    }
+
 }
 
 let app = new App();
 app.runApplication();
+app.newClassFunction();
+app.anotherFunctionWithArguments("mijn argument is iets...");
