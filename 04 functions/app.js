9449ba35de11867d2532ff2a6d2c3da687d3fef3
Yati <37732@ma-web.nl>
2023-12-04 14:34:05 +0100

    04 3

--- 04 functions/app.js
+++ 04 functions/app.js
@@ -1,28 +1,52 @@
-function argumentsAreHandy(shoutout)
+function heeftEenResultaat()
 {
-    console.log("do you want to give a shoutout?");
-    console.log(shoutout);
+    let resultaat=1;
+    return resultaat;
 }
-argumentsAreHandy("Super Shout out");
-argumentsAreHandy("Wow omg");
-argumentsAreHandy("Shouting out");
 
+let hetResultaat = heeftEenResultaat();
+console.log(hetResultaat);
+console.log(heeftEenResultaat());
 
-function superMooieGlobalFunction()
+let x = 9;
+let a = 3;
+let b = 4;
+let c = 89;
+
+let y = (a*(x*x))+(b*x)+c;
+console.log(y);
+
+function ax2bcWiskunde(x,a,b,c)
 {
-    console.log("ik ben global");
-    console.log("dus je mag mi overal aanroepen");
+    let y = (a*(x*x))+(b*x)+c;
+    return y;
 }
-superMooieGlobalFunction();
-superMooieGlobalFunction();
-superMooieGlobalFunction();
+
+let y1 = ax2bcWiskunde(9,3,4,89);
+console.log(y1);
+
+let y2 = ax2bcWiskunde(3,6,5,45);
+console.log(y2);
+
+let y3 = ax2bcWiskunde(7,1,2,13);
+console.log(y3);
+
+function logY(u,h,l)
+{
+
+    let logY = Math.log(l) + h + Math.pow(u,2);
+    return(logY);
+}
+
+let y4 = logY(9,8,15);
+console.log(y4);
+
 
 class App
 {
     runApplication()
     {
         console.log("hello world!");
-        superMooieGlobalFunction();
     }
 }
 
