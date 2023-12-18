bce3d336be1fdbf540f26ab1f2e3b58efc4eb191
Yati <37732@ma-web.nl>
2023-12-18 14:21:14 +0100

    array2

--- 05 arrays/app.js
+++ 05 arrays/app.js
@@ -4,13 +4,35 @@ class App
     {
         console.log("hello world!");
         const favartiest = ['artiest1','artiest2','artiest3'];
-        console.log(favartiest)
         for( let i = 0; i < favartiest.length; i++){
             const element = favartiest[i];
             const artiestrang = (i + 1) + ": " + element;
+            
             console.log(artiestrang);
         }
-
+        favartiest.push("bob dylan","prince")
+        for( let i = 0; i < favartiest.length; i++){
+            const element = favartiest[i];
+            const artiestrang = (i + 1) + ": " + element;
+            
+            console.log(artiestrang);
+        }
+        favartiest.splice(3,4)
+        for( let i = 0; i < favartiest.length; i++){
+            const element = favartiest[i];
+            const artiestrang = (i + 1) + ": " + element;
+            
+            console.log(artiestrang);
+        }
+        favartiest.push("artiest4","artiest5")
+        for( let i = 0; i < favartiest.length; i++){
+            const element = favartiest[i];
+            const artiestrang = (i + 1) + ": " + element;
+            
+            console.log(artiestrang);
+        }
+    
+    
         const nummers = [2,5,7]
         console.log(nummers)
         for( let i = 0; i < nummers.length; i++){
