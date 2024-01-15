2097964357746f783122311e215ecc9321b66b0d
Yati <37732@ma-web.nl>
2024-01-15 13:31:13 +0100

    07 5

--- 07 dinos/app.js
+++ 07 dinos/app.js
@@ -5,20 +5,32 @@ class App
         console.log("hello world!");
 
         let dino = new Dino("T-rex",true,20);
+        let plantenEter = new Dino ("Triceratops",false,20);
         console.log(dino);
-        console.log("de leeftijd van deze " + dino.naam + " is: " + dino.leeftijd);
-        console.log("en deze " + dino.naam + " eet vlees: " + dino.vleeseter);
+        console.log(plantenEter);
+        
+        dino.eatFood(plantenEter);
+        console.log("leeft " + plantenEter.naam + "? " + plantenEter.leeft);
+        
     }
 }
 class Dino
 {
     constructor(naam)
     {
-        this.naam = "T-rex";
+        this.naam = naam;
         this.vleeseter = true;
         this.leeftijd = 20;
-
+        this.leeft = true;
+        this.honger = true;
        
+    }
+    eatFood(foodToEat)
+    {
+        foodToEat.leeft=false;
+        this.honger=false;
+        console.log("ik ben een " + this.naam)
+        console.log("mijn honger: " + this.honger)
     }
 }
 
